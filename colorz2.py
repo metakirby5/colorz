f88a3a526a74ab4e45e0a4dc2904db5ac7c14efc
Ethan Chan <metakirby5@gmail.com>
2015-07-02 23:10:11 -0700

    add brighten, bold colors, html preview

--- colorz2.py
+++ colorz2.py
@@ -6,7 +6,7 @@ from colorsys import rgb_to_hsv, hsv_to_rgb
 
 THUMB_SIZE = (200, 200)
 COLORS = [ImageColor.getrgb(s) for s in [
-  'red', 'green', 'yellow', 'blue', 'magenta', 'cyan',
+  'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white',
 ]]
 SCALE = 256.0
 down_scale = lambda x: x / SCALE
@@ -23,18 +23,27 @@ def get_colors(img):
     for count, color in img.getcolors(w * h)
   )))
 
-def normalize(color, min_v=0, max_v=256):
-  print color
+def clamp(color, min_v, max_v):
+  """
+  Clamps a color such that the value is between min_v and max_v.
+  """
   h, s, v = rgb_to_hsv(*map(down_scale, color))
   min_v, max_v = map(down_scale, (min_v, max_v))
   v = min(max(min_v, v), max_v)
-  print "%s %s %s" % (h, s, v)
   return tuple(map(up_scale, hsv_to_rgb(h, s, v)))
 
-def colorz(filename, min_v=80, max_v=160):
+def brighten(color, brightness):
+  """
+  Adds or subtracts value to a color.
+  """
+  h, s, v = rgb_to_hsv(*map(down_scale, color))
+  return tuple(map(up_scale, hsv_to_rgb(h, s, v + down_scale(brightness))))
+
+def colorz(filename, min_v=80, max_v=160, bold_add=80):
   """
   Get the dominant colors of an image, using the terminal colors
-  as guesses. Normalizes value to between min_v and max_v.
+  as guesses. Clamps value to between min_v and max_v.
+  Creates bold colors using bold_add.
   """
   img = Image.open(filename)
   img.thumbnail(THUMB_SIZE)
@@ -42,4 +51,29 @@ def colorz(filename, min_v=80, max_v=160):
   obs = array(get_colors(img))
   guess = array(COLORS)
   clusters, _ = kmeans(obs, guess)
-  return [normalize(color, min_v, max_v) for color in clusters]
+  clamped = [clamp(color, min_v, max_v) for color in clusters]
+  return reduce(
+    lambda l, c: l + [c, brighten(c, bold_add)],
+    clamped, []
+  )
+
+def html_preview(filename, colors, bg='#272727'):
+  """
+  Creates an html preview of each color.
+  """
+  with open(filename, 'w') as f:
+
+    # Create the main body
+    body = '\n'.join(map(lambda c: """
+      <div style="color: {0}">
+        {0}
+      </div>
+    """.format(hexify(c)), colors))
+
+    # Write the file
+    f.write("""
+      <body style="background-color: {bg}; font-size: 2em">
+        {body}
+      </body>
+    """.format(bg=bg, body=body))
+
